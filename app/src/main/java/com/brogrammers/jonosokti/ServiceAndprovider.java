6d8e3f44daed56cd2150e6a3069d1d529f877a19
Iftakher Hasan <iftakherhasan1@gmail.com>
2020-10-06 13:50:27 +0600

    sampleview service and provider added

---
+++ app/src/main/java/com/brogrammers/jonosokti/ServiceAndprovider.java
@@ -0,0 +1,14 @@
+package com.brogrammers.jonosokti;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ServiceAndprovider extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_service_andprovider);
+    }
+}
